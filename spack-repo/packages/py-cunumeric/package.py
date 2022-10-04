08b8d17ddfc32a26ce654ad604b588b64fb1aeec
Irina Demeshko <idemeshko@nvidia.com>
2022-10-03 20:48:20 -0600

    more work on spack

--- spack-repo/packages/py-cunumeric/package.py
+++ spack-repo/packages/py-cunumeric/package.py
@@ -1,15 +1,16 @@
-from spack import *
+
+from spack.package import *
 
 
-class Cunumeric(CMakePackage, CudaPackage):
+class PyCunumeric(PythonPackage):
     '''cuNumeric is a Legate library that aims to provide a distributed
     and accelerated drop-in replacement for the NumPy API on top of the
     Legion runtime.
     '''
     homepage = 'https://nv-legate.github.io/cunumeric/index.html'
-    git      = 'https://github.com/nv-legate/cunumeric'
+    git      = 'https://github.com/nv-legate/cunumeric.git'
 
-    version('22.10', branch='branch-22.10', submodules=False, preferred=False)
+    version('22.10', branch='branch-22.10')
 
 
     #--------------------------------------------------------------------------#
@@ -28,88 +29,42 @@ class Cunumeric(CMakePackage, CudaPackage):
     variant('shared', default=True,
             description='Build Shared Libraries')
 
+    variant('openmp', default=True,
+            description='Build with OpenMP support')
+
+    variant('cuda', default=True,
+            description='Build with CUDA support')
+
     #variant ('cuda_arch', default=None, description = 'Cuda architecture')
 
     #--------------------------------------------------------------------------#
     # Dependencies
     #--------------------------------------------------------------------------#
 
-    # CMake
-
     depends_on('cmake@3.24:')
-
-    #python
-
-    depends_on('python@3.10')
-
-    #pip
-
+    depends_on('python@3.8:',type=("build", "run"))
     depends_on('py-pip')
-
     depends_on('py-scikit-build')
-
-    #git
-
-    depends_on('git')
-
-    #zlib
-
-    depends_on('zlib')
-
-    #ninja
-
+    #depends_on('git')
+    #depends_on('zlib')
     depends_on('ninja')
-
-    #openmpi
-
     depends_on('openmpi')
-
-    #scikit-build
-
     depends_on('py-scikit-build')
-
-    #cutensor
-
     depends_on('cutensor@1.3.3:')
 
-    #nccl
+#    cuda_arch_list = ('60', '70', '75', '80', '86')
+#    for _flag in cuda_arch_list:
+#        depends_on("nccl cuda_arch=" + _flag, when=" cuda_arch=" + _flag)
 
-    cuda_arch_list = ('60', '70', '75', '80', '86', '89', '90')
-    for _flag in cuda_arch_list:
-        depends_on("nccl cuda_arch=" + _flag, when=" cuda_arch=" + _flag)
-    #depends_on('nccl cuda_arch=' )
-
-    #setuptools
-
-    depends_on('py-setuptools@59:')
-
-    #cffi
-
+    depends_on('py-setuptools@59:',type=("build", "run"))
     depends_on('py-cffi')
-
-    #re2
-
     depends_on('re2')
     depends_on('pcre2')
-
-    #numpy
-
     depends_on('py-numpy@1.22:')
-
-    #opt_einsum
-
     depends_on('py-opt-einsum')
-
-    #pyarrow
-
+    depends_on('utf8proc@2.2:')
     depends_on('py-pyarrow@5:')
-
-    #scipy
-
     depends_on('py-scipy')
-
-    #typing_extensions
-
     depends_on('py-typing-extensions')
 
     #clang
@@ -126,78 +81,39 @@ class Cunumeric(CMakePackage, CudaPackage):
 
     #depends_on('clang-tools@8:', when='+tests')
 
-    #colorama
     
     depends_on('py-colorama', when='+tests')
-    
-    #coverage
-    
     depends_on('py-coverage', when='+tests')
-
-    #mock
-    
     depends_on('py-mock', when='+tests')
-
-    #mypy
-
     depends_on('py-mypy@0.961:', when='+tests')
-
-    #pre-commit
-
     depends_on('py-pre-commit', when='+tests')
-
-    #pynvml
-
     depends_on('py-pynvml', when='+tests')
-
-    #pytest
-
     depends_on('py-pytest', when='+tests')
-
-    #pytests-cov
-
     depends_on('py-pytest-cov', when='+tests')
-
-    #pytest-lazy-fixture
-
     #depends_on('py-pytest-lazy-fixture', when='+tests')
-
-    #types-docutils
-
     depends_on('py-docutils', when='+tests')
 
     #Docs
 
-    #jinja2
-
     depends_on('py-jinja2', when='+docs')
-
-    #pydata-sphinx-theme
-
     #depends_on('py-pydata-sphinx-theme', when='+docs')
-
-    #recommonmark
-    
     depends_on('py-recommonmark', when='+docs')
-
-    #markdown
-
     depends_on('py-markdown@:3.4.0', when='+docs')
-
-    #sphinx
     depends_on('py-sphinx@4.4.0:', when='+docs')
-
-    #sphinx-copybutton
-
     depends_on('py-sphinx-copybutton', when='+docs')
-
-    #sphinx-markdown-tables
-
     #depends_on('py-sphinx-markdown-tables', when='+docs')
-
-    # Graphviz
-
     depends_on('graphviz', when='+prof')
 
 
+    #FIXME
+    depends_on('py-legate-core +shared cuda_arch=80')
+#    depends_on('legion@cr network=gastet conduit=mpi  +python +cuda +openmpi +redop_complexi +bindings arch=80')
+
+ #   def global_options(self, spec, prefix):
+ #       options = []
+ #       if '+cuda' in spec:
+ #           options.append('--cuda')
+ #       if 'openmp' in spec:
+ #           options.append('--openmp')
+ #       return options
 
---
+++ spack-repo/packages/py-legate-core/package.py
@@ -0,0 +1,119 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install legate-core-git
+#
+# You can edit this file again by typing:
+#
+#     spack edit legate-core-git
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack.package import *
+
+
+class PyLegateCore(PythonPackage):
+    """FIXME: Put a proper description of your package here."""
+
+    homepage = "https://nv-legate.github.io/legate.core/index.html"
+    git = "https://github.com/nv-legate/legate.core.git"
+
+    maintainers = ["ipdemes"]
+
+    version('22.10', branch = "branch-22.10")
+
+
+    #--------------------------------------------------------------------------#
+    # Variants
+    #--------------------------------------------------------------------------#
+
+    variant('tests', default=False,
+            description='Enable Unit Tests ')
+
+    variant('docs', default=False,
+            description='Enable Documentation ')
+
+    variant('prof', default=False,
+            description='Enable Prof output')
+
+    variant('shared', default=False,
+            description='Build Shared Libraries')
+
+    variant('openmp', default=True,
+            description='Build with OpenMP support')
+
+    variant('cuda', default=True,
+            description='Build with CUDA support')
+
+    variant ('cuda_arch', default=80, description = 'Cuda architecture')
+
+    variant('shared', default=True,
+            description='Build Shared Libraries')
+
+
+    #--------------------------------------------------------------------------#
+    # Dependencies
+    #--------------------------------------------------------------------------#
+
+    depends_on('cmake@3.24:')
+    depends_on('python@3.8:',type=("build", "run"))
+    depends_on('py-pip')
+    depends_on('py-scikit-build')
+    depends_on('ninja')
+    depends_on('openmpi')
+    depends_on('py-scikit-build')
+    depends_on('cutensor@1.3.3:')
+
+    cuda_arch_list = ('60', '70', '75', '80', '86')
+    for _flag in cuda_arch_list:
+        depends_on("nccl cuda_arch=" + _flag, when=" cuda_arch=" + _flag)
+
+    depends_on('py-setuptools@59:')
+    depends_on('py-cffi')
+    depends_on('re2')
+    depends_on('pcre2')
+    depends_on('py-numpy@1.22:')
+    depends_on('py-opt-einsum')
+    #depends_on('py-pyarrow@5:')
+    depends_on('py-scipy')
+    depends_on('py-typing-extensions')
+
+    depends_on('py-colorama', when='+tests')
+    depends_on('py-coverage', when='+tests')
+    depends_on('py-mock', when='+tests')
+    depends_on('py-mypy@0.961:', when='+tests')
+    depends_on('py-pre-commit', when='+tests')
+    depends_on('py-pynvml', when='+tests')
+    depends_on('py-pytest', when='+tests')
+    depends_on('py-pytest-cov', when='+tests')
+    #depends_on('py-pytest-lazy-fixture', when='+tests')
+    depends_on('py-docutils', when='+tests')
+
+    depends_on('legion@cr network=gasnet conduit=mpi  +python +cuda +openmp +redop_complex +bindings +shared ')
+    # FIXME: Add dependencies if required.
+    # depends_on("foo")
+
+    def install_options(self, spec, prefix):
+        options = []
+        if '+cuda' in spec:
+            options.append('--cuda')
+        if 'openmp' in spec:
+            options.append('--openmp')
+        return options
+
+
+#    def install(self, spec, prefix):
+#        # FIXME: Unknown build system
+#        make()
+#        make("install")
